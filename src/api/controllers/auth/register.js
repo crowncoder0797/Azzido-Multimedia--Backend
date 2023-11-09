201dfc5cf1d64179174aa11b3084738fc39b6384
crowncoder0797 <areejsz124@gmail.com>
2023-11-10 04:50:05 +0900

    initial commit

---
+++ src/api/controllers/auth/register.js
@@ -0,0 +1,36 @@
+import User from '../../models/User.js'
+import Role from '../../models/Role.js'
+import UserRole from '../../models/UserRole.js'
+import { generateToken } from '../../../utils/auth.js'
+
+export const register = async (req, res) => {
+  try {
+    const firstname = req.body.firstname
+    const lastname = req.body.lastname
+    const email = req.body.email.toLowerCase()
+    const password = req.body.password
+
+    const user = await User.findOne({ email })
+
+    if (user) {
+      return res.status(400).send({ error: 'Failed! Email is already in use'})
+    }
+
+    const name = `${firstname} ${lastname}`
+
+    const newUser = await User.create({
+      name: name,
+      email: email,
+      password: password,
+      confirmed: true
+    })
+
+    const authenticatedRole = await Role.findOne({type: 'AUTHENTICATED'})
+
+    await UserRole.create({user: newUser._id, role: authenticatedRole._id})
+
+    return res.status(200).send(newUser)
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+}
